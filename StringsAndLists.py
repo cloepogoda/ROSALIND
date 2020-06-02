48d9cbbd3754a8d2b6b3f7bc8cb41af1936a33cd
Cloe Pogoda <pogoda@pogoda.colorado.edu>
2020-06-02 11:54:30 -0600

    problemthreeRosalind

---
+++ StringsAndLists.py
@@ -0,0 +1,8 @@
+import sys
+s="1wznlFBoigaK4nGmouTvLu2y33IizKAkwbslKGsRPHYFQBo10bDnMN87Va5XvjspXBo1Dkd3jirALfvGxCjSxnXwh56IQJ2PbDxMu86XaZYoK2HsczttKkdmlfrZ0bfTDvMYksKFZmz1upIEMhipposideroshksf4p8."
+a=6
+b=10
+c=145
+d=156
+
+print s[a:b+1] + " " + s[c:d+1]
